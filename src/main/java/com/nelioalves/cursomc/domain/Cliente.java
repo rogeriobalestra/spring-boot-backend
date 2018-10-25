e14064debe51eb819826cdc2aeffcc0b1e99db30
rbalestra <rogeriobalestra@gmail.com>
2018-10-25 10:58:32 -0300

    Pedido, Estado Pagamento e pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -38,6 +38,11 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+	
 	public Cliente() {
 		
 	}
@@ -107,6 +112,15 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -131,7 +145,8 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+
 	
 	
 	
